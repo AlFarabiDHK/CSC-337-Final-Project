36726f0d42fc99f51c55eea09da1f53a1115ab18
AlFarabi <muhtasimalfarabi@email.arizona.edu>
2021-12-04 17:58:13 -0700

    Edit request

--- Final Project/server.js
+++ Final Project/server.js
@@ -201,6 +201,14 @@ app.get('/welcome/:username', (req, res) => {
   })
 });
 
+app.get('/edit/:username', (req, res) => {
+  Freelancer.findOne({username : req.params.username})
+    .exec(function (err, results) {
+      if (err) return handleError(err);
+    console.log(results._id);
+  });
+  //implement update document
+  });
 
 
 app.get('/logout/', (req, res) => {
